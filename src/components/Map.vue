3d0a196ef0007b67da64194fb3907c25b8a4ec16
Vladi Benesova <vladi.benesova1@gmail.com>
2020-06-13 12:34:34 +0200

    add map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="mapFilters">
       <div class="selectedFilters"></div>
-      <div id="map"></div>
+      <div class="mapa" id="map"></div>
     </div>
 
     <div class="picsCafe">
@@ -25,7 +25,28 @@
     </div>
   </div>
 </template>
+<script>
+export default {
+  name: "Map",
+  data() {
+    return {};
+  },
+  mounted() {
+    let main = document.querySelector("#map");
+    console.log(main);
+    let center = SMap.Coords.fromWGS84(14.4179, 50.12655);
+    let map = new SMap(main, center, 13);
+    map.addDefaultLayer(SMap.DEF_BASE).enable();
+    map.addDefaultControls();
+  },
+};
+</script>
 <style>
+.mapa {
+  width: 80vw;
+  height: 80vw;
+  margin: 0 auto;
+}
 .picsCafe {
   display: flex;
   flex-direction: column;
