b6707e7e77c7a85a694dc5c5a703b87ec8e7116a
Marketa Krucka <marketakrucka@gmail.com>
2020-06-17 21:19:59 +0200

    changed some bootstrap to css in component filter

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -3,42 +3,42 @@
   <h1 class="m-3">Select filters</h1>
   <div class="filterList d-flex flex-column flex-md-row flex-md-wrap align-items-center justify-content-center">
 
-    <div class="icon btn btn-light m-2 p-2 pl-4 d-flex align-items-center justify-content-left pl-md-5"
+    <div class="icon btn btn-light"
           @click="milkSelect"
          :class="{'selected': milkSelected}">
       <img src="../assets/img/milk.png" alt="VegiMilk">Alternative milk</div>   
 
-    <div class="icon btn btn-light m-2 p-2 pl-4 d-flex align-items-center justify-content-left pl-md-5"
+    <div class="icon btn btn-light"
          @click="decafSelect"
          :class="{'selected': decafSelected}">
       <img src="../assets/img/decaf.png" alt="Decaf">Decaf coffee</div>
 
-    <div class="icon btn btn-light m-2 p-2 pl-4 d-flex align-items-center justify-content-left pl-md-5"
+    <div class="icon btn btn-light"
          @click="foodSelect"
          :class="{'selected': foodSelected}">
       <img src="../assets/img/soup.png" alt="Hot Food">Hot food</div>
 
-    <div class="icon btn btn-light m-2 p-2 pl-4 d-flex align-items-center justify-content-left pl-md-5"
+    <div class="icon btn btn-light"
          @click="barrierSelect"
          :class="{'selected': barrierSelected}">
       <img src="../assets/img/disabled.png" alt="Wheelchair">Barrier-free</div>
 
-    <div class="icon btn btn-light m-2 p-2 pl-4 d-flex align-items-center justify-content-left pl-md-5"
+    <div class="icon btn btn-light"
          @click="freelanceSelect"
          :class="{'selected': freelanceSelected}">
       <img src="../assets/img/laptop.png" alt="Freelance Friendly">Freelance friendly</div>
 
-    <div class="icon btn btn-light m-2 p-2 pl-4 d-flex align-items-center justify-content-left pl-md-5"
+    <div class="icon btn btn-light"
          @click="familySelect"
          :class="{'selected': familySelected}">
       <img src="../assets/img/baby.png" alt="Family Friendly">Family friendly</div>
 
-    <div class="icon btn btn-light m-2 p-2 pl-4 d-flex align-items-center justify-content-left pl-md-5"
+    <div class="icon btn btn-light"
          @click="petSelect"
          :class="{'selected': petSelected}">
       <img src="../assets/img/pawprint.png" alt="Pet friendly ">Pet friendly</div>
 
-    <div class="icon btn btn-light m-2 p-2 pl-4 d-flex align-items-center justify-content-left pl-md-5"
+    <div class="icon btn btn-light"
          @click="outdoorSelect"
          :class="{'selected': outdoorSelected}">
       <img src="../assets/img/outdoors.png" alt="Outside seating ">Outside seating</div>
@@ -104,6 +104,13 @@ yellow: #f9be60
 .filterComponent {
   margin: 0 auto;
 }
+
+.filterList {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
 img{
  width: 2rem;
  height: 2rem;
@@ -112,6 +119,12 @@ img{
 
 .icon {
   width:100%;
+  margin: 0.5rem;
+  padding: 0.5rem;
+  padding-left: 2rem;
+  display: flex;
+  align-items: center;
+
 }
 .find{
   background-color: #cd6e48;
@@ -127,6 +140,7 @@ img{
  {
   background-color: #ffdd8f;
 }
+
 
 @media only screen and (min-width : 768px)   {
 .icon {
