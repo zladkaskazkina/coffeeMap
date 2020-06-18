cbb48670cb665fee4d70c09c3c4ccd100cb0037b
Vladi Benesova <vladi.benesova1@gmail.com>
2020-06-18 22:08:17 +0200

    add json data to firebase

--- src/index.js
+++ src/index.js
@@ -30,3 +30,26 @@ new Vue({
   router: router,
   render: h => h(App),
 });
+
+//add Json to Firebase
+
+const admin = require('../node_modules/firebase-admin');
+const serviceAccount = require("../serviceAccount.json");
+const data = require("../data.json");
+const collectionKey = "coffeemap-aeaf4"; //name of the collection
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://coffeemap-aeaf4.firebaseio.com"
+});
+const firestore = admin.firestore();
+const settings = { timestampsInSnapshots: true };
+firestore.settings(settings);
+if (data && (typeof data === "object")) {
+  Object.keys(data).forEach(docKey => {
+    firestore.collection(collectionKey).doc(docKey).set(data[docKey]).then((res) => {
+      console.log("Document " + docKey + " successfully written!");
+    }).catch((error) => {
+      console.error("Error writing document: ", error);
+    });
+  });
+}
